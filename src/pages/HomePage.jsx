8809b8088fab8a2643d154c123f4a083cfd28b6f
Lekhana19 <lekhanagadde@gmail.com>
2023-10-19 15:06:13 -0700

    Revert changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,35 +1,5 @@
-import { Container,Paper, makeStyles, createTheme, ThemeProvider, Typography } from "@mui/material";
+import { Container,Paper, Typography } from "@mui/material";
 import React from "react";
-
-
-const theme = createTheme({
-    palette: {
-      primary: {
-        main: '#1976D2', // Customize the primary color
-      },
-      secondary: {
-        main: '#FFC107', // Customize the secondary color
-      },
-    },
-    typography: {
-      h4: {
-        fontWeight: 600, // Make heading text bold
-        marginBottom: '20px', // Add spacing below the heading
-      },
-      body1: {
-        fontSize: '1.2rem', // Increase the font size of the body text
-      },
-    },
-  });
-
-const useStyles = makeStyles((theme) => ({
-    paper: {
-      padding: '20px',
-      marginTop: '20px',
-      backgroundColor: 'rgba(255, 255, 255, 0.9)', // Add a semi-transparent white background
-    },
-  }));
-
 const HomePage = () => {
   return (
     <Container maxWidth="md">
@@ -38,7 +8,7 @@ const HomePage = () => {
         Welcome to Myfilebox
       </Typography>
       <Typography variant="body1">
-        We're delighted to have you here! Explore our content, products, and services. 
+        We're delighted to have you here! Navigate to the login page to explore more about the application
       </Typography>
     </Paper>
   </Container>
