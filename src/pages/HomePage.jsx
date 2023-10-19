95d2df41dee5fafd3ed758c089cc83b064bf2823
Lekhana19 <lekhanagadde@gmail.com>
2023-10-19 15:00:20 -0700

    added colors to homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,35 @@
-import { Container,Paper, Typography } from "@mui/material";
+import { Container,Paper, makeStyles, createTheme, ThemeProvider, Typography } from "@mui/material";
 import React from "react";
+
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#1976D2', // Customize the primary color
+      },
+      secondary: {
+        main: '#FFC107', // Customize the secondary color
+      },
+    },
+    typography: {
+      h4: {
+        fontWeight: 600, // Make heading text bold
+        marginBottom: '20px', // Add spacing below the heading
+      },
+      body1: {
+        fontSize: '1.2rem', // Increase the font size of the body text
+      },
+    },
+  });
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      padding: '20px',
+      marginTop: '20px',
+      backgroundColor: 'rgba(255, 255, 255, 0.9)', // Add a semi-transparent white background
+    },
+  }));
+
 const HomePage = () => {
   return (
     <Container maxWidth="md">
